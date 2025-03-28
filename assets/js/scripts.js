badc41484d3ca0403253d78723265658d3448ace
vicentef9 <v.f.c.g.0202@gmail.com>
2025-03-28 18:16:28 -0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(document).on('click', '.btn-linkedin', function(){
+        window.open("https://www.linkedin.com/in/vicentecossio10/", "_blank");
+    });
+
+    $(document).on('click', '.btn-email', function(){
+        window.location.href = "mailto:v.f.c.g.0202@gmail.com";
+    });
+});
